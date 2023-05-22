310b0f54a0b29a85fd07db8cb9c9e67f4b2c72e0
Xavier <xavierisaac019@gmail.com>
2023-05-22 17:00:00 -0600

    El boton Desencriptar funciona correctamente y se pulio el funcionamiento del boton Encriptar

--- script.js
+++ script.js
@@ -1,29 +1,50 @@
-// Esperar a que se cargue el DOM
-document.addEventListener("DOMContentLoaded", function() {
-  // Obtener referencia al botón
-  var boton1 = document.querySelector(".boton1");
-  // Obtener referencia al elemento de entrada de texto
+function reemplazarVocales(texto) {
+	var reemplazos = {
+    a: "ai",
+   	e: "enter",
+   	i: "imes",
+    o: "ober",
+    u: "ufat"
+}
+	return texto.replace(/[aeiou]/g, function(vocal){
+	return reemplazos[vocal];
+	})
+}
+
+function botonEncriptarClick() {
+	event.preventDefault();
+	var inputTexto = document.querySelector(".cajaTexto input");
+	var resultado = document.querySelector(".cajaResultado");
+	var texto = inputTexto.value.toLowerCase();
+	var textoEncriptado = reemplazarVocales(texto);
+
+	resultado.innerHTML = "<p>" + textoEncriptado + "</p>";
+
+	inputTexto.value = "";
+}
+
+function invertirReemplazos(texto) {
+  var reemplazos = {
+    ai: "a",
+    enter: "e",
+    imes: "i",
+    ober: "o",
+    ufat: "u"
+  };
+  return texto.replace(/ai|enter|imes|ober|ufat/g, function(palabra) {
+    return reemplazos[palabra];
+  });
+}
+
+function botonDesencriptarClick() {
+  event.preventDefault();
   var inputTexto = document.querySelector(".cajaTexto input");
-  // Obtener referencia al elemento para mostrar el texto encriptado
-  var resultado = document.querySelector(".cajaResultado");
+  var resultado = document.querySelector(".cajaResultado p");
+  var texto = inputTexto.value.toLowerCase();
+  var textoDesencriptado = invertirReemplazos(texto);
 
-  // Agregar evento de clic al botón
-  boton1.addEventListener("click", function(event) {
-    // Evitar que el formulario se envíe y la página se recargue
-    event.preventDefault();
+  resultado.textContent = textoDesencriptado;
 
-    // Obtener el texto ingresado por el usuario
-    var texto = inputTexto.value;
+  inputTexto.value = "";
+}
 
-    // Realizar la encriptación reemplazando las vocales
-    var textoEncriptado = texto
-      .replace(/a/g, "ai")
-      .replace(/e/g, "enter")
-      .replace(/i/g, "imes")
-      .replace(/o/g, "ober")
-      .replace(/u/g, "ufat");
-
-    // Mostrar el texto encriptado en lugar de la imagen
-    resultado.innerHTML = "<p>" + textoEncriptado + "</p>";
-  })
-})
