8d08349022d7d7522a91609bef31d85bc0099b01
Xavier <xavierisaac019@gmail.com>
2023-05-23 15:33:38 -0600

    Añadido boton Copiar y pulido funcionamiento del boton Desencriptar

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+//-----------------------Encriptar--------------------------
 function reemplazarVocales(texto) {
+	//Variables para las equivalencias de las vocales
 	var reemplazos = {
     a: "ai",
    	e: "enter",
@@ -20,31 +22,50 @@ function botonEncriptarClick() {
 
 	resultado.innerHTML = "<p>" + textoEncriptado + "</p>";
 
+	var botonCopiar = document.querySelector(".botonCopiar");
+  	botonCopiar.style.display = "block";
+
 	inputTexto.value = "";
 }
 
+//-----------------------Desencriptar--------------------------
 function invertirReemplazos(texto) {
-  var reemplazos = {
-    ai: "a",
-    enter: "e",
-    imes: "i",
-    ober: "o",
-    ufat: "u"
+	//Variables para las equivalencias de las palabras
+	var reemplazos = {
+	    ai: "a",
+	    enter: "e",
+	    imes: "i",
+	    ober: "o",
+	    ufat: "u"
   };
   return texto.replace(/ai|enter|imes|ober|ufat/g, function(palabra) {
     return reemplazos[palabra];
-  });
+  })
 }
 
 function botonDesencriptarClick() {
-  event.preventDefault();
-  var inputTexto = document.querySelector(".cajaTexto input");
-  var resultado = document.querySelector(".cajaResultado p");
-  var texto = inputTexto.value.toLowerCase();
-  var textoDesencriptado = invertirReemplazos(texto);
+	event.preventDefault();
+	var inputTexto = document.querySelector(".cajaTexto input");
+	var resultado = document.querySelector(".cajaResultado");
+	var texto = inputTexto.value.toLowerCase();
+	var textoDesencriptado = invertirReemplazos(texto);
 
-  resultado.textContent = textoDesencriptado;
+	resultado.innerHTML = "<p>" + textoDesencriptado + "</p>";
+  
+	var botonCopiar = document.querySelector(".botonCopiar");
+  	botonCopiar.style.display = "block";
 
   inputTexto.value = "";
 }
 
+//-----------------------Copiar--------------------------
+function botonCopiarClick() {
+	event.preventDefault();
+	var textoResultado = document.querySelector(".cajaResultado p").textContent;
+
+	// Copiar al portapapeles
+	navigator.clipboard.writeText(textoResultado)
+    .catch(function(err) {
+	console.error("Error al copiar el texto: ", err);
+    })
+}
